10f760b25854714029ba46a24b10c223b6e9a398
Cody Stair <ccstair27@gmail.com>
2020-11-14 20:41:01 -0500

    another fix

--- src/App.js
+++ src/App.js
@@ -1,36 +1,31 @@
-import PropTypes from 'prop-types';
-import React from 'react';
+import React from "react";
 
 import {
   BrowserRouter as Router,
   Route,
   Link,
   Redirect,
-  Switch
-} from 'react-router-dom';
+  Switch,
+} from "react-router-dom";
 
 const App = () => (
   <Router>
-    <div
-      className='ui text container'
-    >
-      <h2 className='ui dividing header'>
-        Which body of water?
-      </h2>
+    <div className="ui text container">
+      <h2 className="ui dividing header">Which body of water?</h2>
 
       <ul>
         <li>
-          <Link to='/atlantic'>
+          <Link to="/atlantic">
             <code>/atlantic</code>
           </Link>
         </li>
         <li>
-          <Link to='/pacific'>
+          <Link to="/pacific">
             <code>/pacific</code>
           </Link>
         </li>
         <li>
-          <Link to='/black-sea'>
+          <Link to="/black-sea">
             <code>/black-sea</code>
           </Link>
         </li>
@@ -39,29 +34,32 @@ const App = () => (
       <hr />
 
       <Switch>
-        <Route path='/atlantic/ocean' render={() => (
-          <div>
-            <h3>Atlantic Ocean - Again!</h3>
-            <p>
-              Also known as "The Pond."
-            </p>
-          </div>
-        )} />
-        <Route path='/atlantic' component={Atlantic} />
-        <Route path='/pacific' component={Pacific} />
-        <Route path='/black-sea' component={BlackSea} />
-        <Route exact path='/' render={() => (
-          <h3>
-            Welcome! Select a body of saline water above.
-          </h3>
-        )} />
-        <Route render={({ location }) => (
-          <div className='ui inverted red segment'>
-            <h3>
-              Error! No matches for <code>{location.pathname}</code>
-            </h3>
-          </div>
-        )} />
+        <Route
+          path="/atlantic/ocean"
+          render={() => (
+            <div>
+              <h3>Atlantic Ocean - Again!</h3>
+              <p>Also known as "The Pond."</p>
+            </div>
+          )}
+        />
+        <Route path="/atlantic" component={Atlantic} />
+        <Route path="/pacific" component={Pacific} />
+        <Route path="/black-sea" component={BlackSea} />
+        <Route
+          exact
+          path="/"
+          render={() => <h3>Welcome! Select a body of saline water above.</h3>}
+        />
+        <Route
+          render={({ location }) => (
+            <div className="ui inverted red segment">
+              <h3>
+                Error! No matches for <code>{location.pathname}</code>
+              </h3>
+            </div>
+          )}
+        />
       </Switch>
     </div>
   </Router>
@@ -71,8 +69,7 @@ const Atlantic = () => (
   <div>
     <h3>Atlantic Ocean</h3>
     <p>
-      The Atlantic Ocean covers approximately 1/5th of the
-      surface of the earth.
+      The Atlantic Ocean covers approximately 1/5th of the surface of the earth.
     </p>
   </div>
 );
@@ -81,8 +78,8 @@ const Pacific = () => (
   <div>
     <h3>Pacific Ocean</h3>
     <p>
-      Ferdinand Magellan, a Portuguese explorer, named the ocean
-      'mar pacifico' in 1521, which means peaceful sea.
+      Ferdinand Magellan, a Portuguese explorer, named the ocean 'mar pacifico'
+      in 1521, which means peaceful sea.
     </p>
   </div>
 );
@@ -93,34 +90,31 @@ class BlackSea extends React.Component {
   };
 
   componentDidMount() {
-    this.interval = setInterval(() => (
-      this.setState(prevState => {
-        return {
-          counter: prevState.counter - 1,
-        };
-      }
-    )), 1000);
+    this.interval = setInterval(
+      () =>
+        this.setState((prevState) => {
+          return {
+            counter: prevState.counter - 1,
+          };
+        }),
+      1000
+    );
   }
 
   componentWillUnmount() {
     clearInterval(this.interval);
   }
-  
+
   render() {
     return (
       <div>
         <h3>Black Sea</h3>
         <p>Nothing to sea [sic] here ...</p>
         <p>Redirecting in {this.state.counter}...</p>
-        {
-          (this.state.counter < 1) ? (
-            <Redirect to='/' />
-          ) : null
-        }
+        {this.state.counter < 1 ? <Redirect to="/" /> : null}
       </div>
     );
   }
 }
 
 export default App;
-
