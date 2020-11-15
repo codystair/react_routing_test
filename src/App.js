c1b393219e42a70e4818cfbb78aef6d6ce655b79
Cody Stair <ccstair27@gmail.com>
2020-11-14 21:19:36 -0500

    oops

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import {
 } from "react-router-dom";
 
 const App = () => (
-  <Router basename={${process.env.PUBLIC_URL}}>
+  <Router basename={process.env.PUBLIC_URL}>
     <div className="ui text container">
       <h2 className="ui dividing header">Which body of water?</h2>
 
